41df0a0b22019b309c03225c541ee45f6e530869
Диана Макарова <pr_dev_01@office.df-mdf.com>
2023-07-18 17:23:07 +0300

    Добавила рабочую формулу, добавила div для вывода ответа

--- script.js
+++ script.js
@@ -2,32 +2,47 @@ const thicknessOfTheJumper_Glasses_ONE = document.querySelector(".glasse1");
 const thicknessOfTheJumper_Glasses_TWO = document.querySelector(".glasse2");
 const thicknessOfTheJumper_Glasses_THREE = document.querySelector(".glasse3");
 const thicknessOfTheJumper_Glasses_FOUR = document.querySelector(".glasse4");
+const listOfGlasses = document.querySelector(".list-of-glasses");
+
 let inputHeightVector = document.querySelector(".bevel-vector");
 let inputQuantityJumpers = document.querySelector(".quantity-jumpers");
-let boxConclusion = document.querySelector(".quantity-jumpers");
-const buttonConclusion = document.querySelector(".box-conclusion");
+// let boxConclusion = document.querySelector(".quantity-jumpers");
+let windowSize = document.querySelector(".answer");
+let buttonConclusion = document.querySelector(".button-conclusion");
 
-// let valueGlasses_ONE = 22;
-// let valueGlasses_TWO = 25;
+let valueGlasses_ONE = 22;
+let valueGlasses_TWO = 25;
 let valueGlasses_THREE = 15;
 let valueGlasses_FOUR = 22;
 
+let theThicknessOfTheJumper; // толщина перемычки
 
-let valueGlasses_ONE = {
-    height: inputHeightVector.value,
-    width: 22,
-    quantityJumpers: inputQuantityJumpers.value * this.width,
+for (list of listOfGlasses) {
+    list.addEventListener("click", () => {
+        if (list[0]) {
+            list.value = valueGlasses_ONE;
+            theThicknessOfTheJumper = valueGlasses_ONE;
+            console.log(theThicknessOfTheJumper);
+        } else if (list[1]) {
+            theThicknessOfTheJumper = valueGlasses_TWO;
+            console.log(list[1]);
+        } else if (list[2]) {
+            theThicknessOfTheJumper = valueGlasses_THREE;
+            console.log(list[2]);
+        } else if (list[3]) {
+            theThicknessOfTheJumper = valueGlasses_FOUR;
+            console.log(list[3]);
+        }
+    })
+    
 }
 
-// формула
-// объект1.высота - расположение перемычек / кол-во перемычек + объект1.width
-let conclusion = ((valueGlasses_ONE.height - valueGlasses_ONE.quantityJumpers) / inputQuantityJumpers) + valueGlasses_ONE.width
-
-buttonConclusion.addEventListener("click", () => {
-    boxConclusion.textContent = conclusion;
-})
-
-
+buttonConclusion.addEventListener("click", function() {
+    // формула
+    let d = theThicknessOfTheJumper * +inputQuantityJumpers.value;
+    let x = (+inputHeightVector.value - d) / +inputQuantityJumpers.value + theThicknessOfTheJumper;
+    windowSize.textContent = x;
+    console.log(d);
+});
 
 
-// let valueGlasses_TWO = {}
