b3eb41495ea75a6f303e8b021348d5fd96c96c00
Диана Макарова <pr_dev_01@office.df-mdf.com>
2023-08-29 15:41:02 +0300

    внесли корректировки по коду, и добавился метод преобразования конечного числа в то чтобы после запятой оставалось только 3 цифры

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
-let heightVector = document.querySelector(".bevel-vector");
-let quantityJumpers = document.querySelector(".quantity-jumpers");
-let windowSize = document.querySelector(".answer");
-let makeChanges = document.querySelector(".make-changes");
-let quantityWindow = document.querySelector(".quantity-windows");
 const buttonConclusion = document.querySelector(".button-conclusion");
 const btnToChange = document.querySelector(".btn-to-change");
 const listOfGlasses = document.querySelector(".list-of-glasses");
 
 
-function mainFunction() {
-  let theThicknessOfTheJumper = listOfGlasses[0].value; // толщина перемычки
-  listOfGlasses.addEventListener("change", function() {
-    theThicknessOfTheJumper = this.value;
-    console.log(theThicknessOfTheJumper);
-  });
-  
-  btnToChange.addEventListener("click", function() {
-    // измение значения стекл
-    let option = listOfGlasses.options[4];
-    option.value = makeChanges.value;
-    theThicknessOfTheJumper = makeChanges.value;
-   });
-   
-   
-  buttonConclusion.addEventListener("click", function() {
+let theThicknessOfTheJumper = listOfGlasses[0].value; // толщина перемычки
+let listValues = listOfGlasses.addEventListener("change", function() {
+  theThicknessOfTheJumper = this.value;
+  console.log(theThicknessOfTheJumper);
+});
+
+
+let newValue = btnToChange.addEventListener("click", function() {
+  let makeChanges = document.querySelector(".make-changes");
+  // измение значения стекла
+  let option = listOfGlasses.options[4];
+  option.value = makeChanges.value;
+  theThicknessOfTheJumper = makeChanges.value;
+});
+
+
+let calculateByTheFormula = buttonConclusion.addEventListener("click", function() {
+    let windowSize = document.querySelector(".answer");
+    let quantityJumpers = document.querySelector(".quantity-jumpers");
+    let heightVector = document.querySelector(".bevel-vector");
+    let quantityWindow = document.querySelector(".quantity-windows");
     // формула
     let d = +theThicknessOfTheJumper * +quantityJumpers.value;
     let x = (+heightVector.value - d) / quantityWindow.value + +theThicknessOfTheJumper;
-    windowSize.textContent = x;
-  });
+    if(+quantityJumpers.value > +quantityWindow.value) {
+      windowSize.textContent = "Ошибка";
+    }
+    windowSize.textContent = x.toFixed(3);
     
-}
-mainFunction();
+  });
+  
+
+// glassSelectionAndItsValues(),  addANewValueForTheJumper()
